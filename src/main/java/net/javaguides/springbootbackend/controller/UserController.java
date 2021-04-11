8e5469a9a86e3608c864ff4899ce374aa69b6ac7
joelbb24 <joelanilraj2410@gmail.com>
2021-04-11 02:23:58 -0400

    First

---
+++ src/main/java/net/javaguides/springbootbackend/controller/UserController.java
@@ -0,0 +1,26 @@
+package net.javaguides.springbootbackend.controller;
+
+import net.javaguides.springbootbackend.model.User;
+import net.javaguides.springbootbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+}
